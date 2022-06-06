c96d35c05be1f9d5667ff3da4569883cf422cab9
ankitakalgut <39291073+ankitakalgut@users.noreply.github.com>
2022-06-06 17:52:47 +0530

    Update ArrayList2.java

--- src/ArrayList2.java
+++ src/ArrayList2.java
@@ -7,7 +7,7 @@ public class ArrayList2 {
 	public static void main(String args[]) {
 		ArrayList<String> al = new ArrayList<String>();
 		al.add("Raviraj");
-		al.add("Vijay");
+		al.add("Vijaya");
 		al.add("Ravi");
 		al.add("Ajay");
 		Iterator<String> itr = al.iterator();
